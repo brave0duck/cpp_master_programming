e9bcd3078f6e03cbe19787a081d79174fbbebacd
brave0duck <newkemp@naver.com>
2026-02-20 00:35:15 +0900

    class test

---
+++ 03/exam/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Tower{
+    int height;
+public:
+    Tower() : Tower(1){}
+    Tower(int n){
+        Tower::height = n;
+    }
+    int getHeight(){
+        return Tower::height;
+    }
+};
+
+int main(){
+    Tower myTower;
+    Tower seoulTower(100);
+    cout << "높이는 " << myTower.getHeight() << "미터" << endl;
+    cout << "높이는 " << seoulTower.getHeight() << "미터" << endl;
+}
