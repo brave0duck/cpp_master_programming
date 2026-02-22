e77f86d57d4927a99d66df77d73f8a0ba60fdfab
brave0duck <newkemp@naver.com>
2026-02-23 01:03:47 +0900

    11.(3)

--- 05/exam/11.cpp
+++ 05/exam/11.cpp
@@ -1,25 +1,46 @@
+// (3)까지 완료. 에러없이 작동은함
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
 
 class Book{
     char * title;
     int price;
 public:
+    Book();
     Book(const char *title, int price);
+    Book(const Book &b);
     ~Book();
-    void set(char* title,int price);
+    void set(const char* title,int price);
     void show(){cout << title << " , " << price << "원" << endl;}
+};
+
+Book::Book(){
+    this->title = NULL;
+    this->price =0;
 }
 Book::Book(const char* title, int price){
-    this->title = title;
+    this->title = new char[strlen(title)+1];
+    strcpy(this->title,title);
     this->price = price;
 }
-Book::~Book(){
-
+Book::Book(const Book &b){
+    this->title = new char[strlen(b.title)+1];
+    strcpy(this->title, b.title);
+    this->price = b.price;
 }
-void Book::set(char* title, int price){
-
+Book::~Book(){
+    if(this->title){
+        delete [] this->title;
+    }
+}
+void Book::set(const char* title, int price){
+    if(this->title){
+        delete [] this->title;
+    }
+    this->title = new char[strlen(title)+1];
+    strcpy(this->title, title);
+    this->price = price;
 }
 int main(){
     Book cpp("명품 C++", 10000);
